60c520a7d1e275330d1f17cb4357ddaf267a3e19
luckygarg019-stac <luckygarg019@gmail.com>
2026-08-07 15:02:58 +0530

    sample file added

---
+++ Sample.java
@@ -0,0 +1,33 @@
+// public class Sample {
+//     // static int x=10;
+//     public static void main(String[] args) {
+//         System.out.println("hello world");
+//     } 
+
+    
+// }
+
+
+// class Sample2{
+//     void display() {
+//         Sample.x=20;
+//         System.out.println("this is Sample 2 class");
+//     }
+// }
+
+
+public class Sample {
+    static int x=10;
+    public static void main(String[] args) {
+        System.out.println("Hello,World");
+        Sample2 obj = new Sample2();
+        obj.display();
+    }
+}
+
+class Sample2 {
+    void display() {
+        Sample.x=20;
+        System.out.println("this is sample2 class");
+    }
+}
